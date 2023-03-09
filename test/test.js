160a56373a765e554b0f43d04e81102da0cb00bf
Steven Crader <stevencrader@gmail.com>
2023-03-08 22:06:21 -0800

    examples

--- test/test.js
+++ test/test.js
@@ -1,5 +1,10 @@
 const lib = require("../dist/index.js")
 
+TRANSCRIPT_HTML_BUZZCAST = "test_files/buzzcast.html"
+TRANSCRIPT_SRT_BUZZCAST = "test_files/buzzcast.srt"
+TRANSCRIPT_JSON_BUZZCAST = "test_files/buzzcast.json"
+TRANSCRIPT_HTML_PODCASTING_20 = "test_files/podcasting_20.html"
+
 test("Example Test", () => {
     lib.transcriptator()
 });
