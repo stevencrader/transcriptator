6c538bac128c6db218fc64d8fcc115bf110a7f20
Steven Crader <stevencrader@gmail.com>
2023-03-10 19:31:55 -0800

    more

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,52 @@
-export const transcriptator = () => {
-    console.log("called!")
+export enum TranscriptType {
+    TEXT_HTML = "text/html",
+    APPLICATION_JSON = "application/json",
+    APPLICATION_SRT = "application/srt",
+    TEXT_SRT = "text/srt",
+    TEXT_VTT = "text/vtt",
+    APPLICATION_X_SUBRIP = "application/x-subrip",
 }
+
+export type Segment = {
+    startTime: number
+    endTime?: number
+    speaker?: string
+    body: string
+}
+
+export type JSONTranscript = {
+    version: string
+    segments: Array<Segment>
+}
+
+const parseHTML = (data: string):Array<Segment> => {
+    const outSegments = []
+    console.log('parseHTML')
+    return outSegments
+}
+
+const parseSRT = (data: string): Array<Segment> => {
+    const outSegments = []
+    console.log('parseSRT')
+    return outSegments
+}
+
+export const convertFile = (data: string, transcriptType: TranscriptType) => {
+    switch (transcriptType) {
+        case TranscriptType.TEXT_HTML:
+            parseHTML(data)
+            break
+        case TranscriptType.APPLICATION_JSON:
+            break
+        case TranscriptType.TEXT_SRT:
+            parseSRT(data)
+            break
+        default:
+            throw new TypeError(`Unknown transcript type: ${transcriptType}`)
+    }
+}
+
+// converter
+// Optional re-format length
+
+// detect format by looking at file contents
--- test/test.js
+++ test/test.js
@@ -1,10 +1,22 @@
 const lib = require("../dist/index.js")
+const fs = require('fs')
 
 TRANSCRIPT_HTML_BUZZCAST = "test_files/buzzcast.html"
-TRANSCRIPT_SRT_BUZZCAST = "test_files/buzzcast.srt"
 TRANSCRIPT_JSON_BUZZCAST = "test_files/buzzcast.json"
-TRANSCRIPT_HTML_PODCASTING_20 = "test_files/podcasting_20.html"
+TRANSCRIPT_SRT_BUZZCAST = "test_files/buzzcast.srt"
+TRANSCRIPT_SRT_PODCASTING_20 = "test_files/podcasting_20.srt"
 
-test("Example Test", () => {
-    lib.transcriptator()
-});
+const readFile = (filePath) => {
+    return fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) throw err
+        console.error(err)
+    })
+}
+
+test("Parse HTML", () => {
+    lib.convertFile(readFile(TRANSCRIPT_HTML_BUZZCAST), lib.TranscriptType["TEXT_HTML"])
+})
+
+test("Parse SRT", () => {
+    lib.convertFile(readFile(TRANSCRIPT_SRT_PODCASTING_20), lib.TranscriptType["TEXT_SRT"])
+})
